1ef40f029fa32bc8becee53d4992a1162d56d47c
deviland91 <as.matias91@gmail.com>
2018-02-27 23:13:58 +0000

    Added none versioned files

---
+++ Pickpocket3D/Assets/SpawnManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    // Game Objects to spawn the ground
+    [Header("Ground to spawn")]
+    public List<GameObject> spawnGround;
+
+    [HideInInspector]
+    public List<GameObject> spawnClone;
+
+    //Game Objects for the enemies to spawn
+    [Header("Civilians to spawn")]
+    public List<GameObject> civilianSpawn;
+
+    [HideInInspector]
+    public List<GameObject> civilianSpawnClone;
+
+    [Header("Guards to spawn")]
+    public List<GameObject> guardSpawn;
+
+    [HideInInspector]
+    public List<GameObject> guardSpawnClone;
+
+    [Header("Buildings to spawn")]
+    public List<GameObject> buildingSpawn;
+
+    [HideInInspector]
+    public List<GameObject> buildingSpawnClone;
+
+    //Player Transform
+    public GameObject player;
+
+    //Spawner Area
+    public GameObject spawner;
+
+    //Last Z size of the ground spawn
+    float lastZGSize = 0;
+    float lastZBSize = 0;
+
+    //Last Z location of the ground spawn
+    float zGLocation = 0;
+    float zBLocation = 0;
+
+    //Enemy spawn location
+    Vector3 enemySpawnLocation = new Vector3(0, 0, 0);
+
+    //Start spawn distance
+    [Range(40, 400)]
+    float spawnDistance = 100;
+
+    Vector3 groundSpawnLocation = new Vector3(0, 0, 0);
+    Vector3 buildingSpawnLocation = new Vector3(0, 0, 0);
+    int x, y, z, toSpawn, amountSpawn = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        
+
+        // Spawn initial ground
+        foreach (GameObject o in spawnGround)
+        {
+            groundSpawnLocation = new Vector3(0, 0, lastZGSize+zGLocation);         
+            spawnClone[x] = Instantiate(spawnGround[x], groundSpawnLocation, Quaternion.Euler(0, 90, 0));
+            lastZGSize = spawnClone[x].GetComponent<Renderer>().bounds.size.z;
+            zGLocation = spawnClone[x].GetComponent<Transform>().position.z;
+            x++;
+        }
+
+        // Spawn initial buildings
+        foreach (GameObject p in buildingSpawn)
+        {
+            buildingSpawnLocation = new Vector3(spawnClone[y].GetComponent<Renderer>().bounds.size.x, 0, lastZBSize + zBLocation);
+            buildingSpawnClone.Add(Instantiate(buildingSpawn[y], buildingSpawnLocation, Quaternion.Euler(0, 90, 0)));
+            lastZBSize = buildingSpawnClone[y].GetComponent<Renderer>().bounds.size.z;
+            zBLocation = buildingSpawnClone[y].GetComponent<Transform>().position.z;
+            y++;
+        }
+
+        InvokeRepeating("spawnGroundFunc", 1, 1);
+        InvokeRepeating("spawnBuildingFunc", 1, 1);
+        InvokeRepeating("spawnPeopleFunc", 1, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+   void spawnGroundFunc()
+    {
+        if (groundSpawnLocation.z - player.transform.position.z <= spawnDistance && spawnGround.Count != 0)
+        {
+            int randSpawnGround = Random.Range(0, spawnGround.Count);
+            groundSpawnLocation = new Vector3(0, 0, lastZGSize + zGLocation);
+            spawnClone.Add(Instantiate(spawnGround[randSpawnGround], groundSpawnLocation, Quaternion.Euler(0, 90, 0)));
+            lastZGSize = spawnClone[x].GetComponent<Renderer>().bounds.size.z;
+            zGLocation = spawnClone[x].GetComponent<Transform>().position.z;
+            x++;
+        }
+    }
+
+    void spawnBuildingFunc()
+    {
+        if (groundSpawnLocation.z - player.transform.position.z <= spawnDistance && spawnGround.Count != 0)
+        {
+            int randSpawnBuilding = Random.Range(0, buildingSpawn.Count);
+            buildingSpawnLocation = new Vector3(0, 0, lastZBSize + zBLocation);
+            buildingSpawnClone[y] = Instantiate(buildingSpawn[y], buildingSpawnLocation, Quaternion.Euler(0, 90, 0));
+            lastZBSize = buildingSpawnClone[y].GetComponent<Renderer>().bounds.size.z;
+            zBLocation = buildingSpawnClone[y].GetComponent<Transform>().position.z;
+            y++;
+        }
+    }
+
+    void spawnPeopleFunc()
+    {
+        amountSpawn = Random.Range(0, 4);
+        while (amountSpawn > 0)
+        {
+
+            enemySpawnLocation = new Vector3(Random.Range(spawner.transform.position.x, spawner.transform.position.x + spawner.GetComponent<BoxCollider>().size.x), 1f, Random.Range(spawner.transform.position.z, spawner.transform.position.z + spawner.GetComponent<BoxCollider>().size.z));
+            toSpawn = Random.Range(0, 100);
+            //Spawn civilian
+            if (toSpawn <= 75)
+            {
+                if (Random.Range(0, 4) <= 3)
+                {
+                    civilianSpawnClone.Add(Instantiate(civilianSpawn[Random.Range(0, civilianSpawn.Count - 1)], enemySpawnLocation, Quaternion.Euler(0, 0, 0)));
+                }
+                else
+                {
+                    civilianSpawnClone.Add(Instantiate(civilianSpawn[Random.Range(0, civilianSpawn.Count - 1)], enemySpawnLocation, Quaternion.Euler(0, 0, 0)));
+                }              
+            }
+            //Spawn Guard
+            else if(toSpawn > 75 && toSpawn <= 100)
+            {
+                guardSpawnClone.Add(Instantiate(guardSpawn[Random.Range(0, guardSpawn.Count - 1)], enemySpawnLocation, Quaternion.Euler(0, 0, 0)));
+            }
+            z++;
+            amountSpawn--;
+        }
+    }
+}
---
+++ Pickpocket3D/Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform player;
+    public float distSpawnPlayer = 100;
+    Vector3 spawnDistance;
+    public Vector3 spawnerSize = new Vector3(15, 0, 15);
+
+    // Use this for initialization
+    void Start () {
+        spawnDistance = new Vector3(0, 0, distSpawnPlayer);
+        BoxCollider spawner = GetComponent<BoxCollider>();
+        spawner.size = spawnerSize;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + spawnDistance;
+
+    }
+}
