ecc665cb031de24d93891d000a3dfaf14bb5ded8
Florian Dhesse <fdhesse@gmail.com>
2018-03-02 19:57:23 +0800

    udpated all player movement

---
+++ Pickpocket3D/Assets/Scripts/AI/MovingAI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingAI : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Pickpocket3D/Assets/Scripts/Player/PlayerStraffing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStraffing : MonoBehaviour {
+    public float Speed = 10;
+    private float Strafe;
+    //private bool right = false;
+    //private bool left = false;
+
+    void Start()
+    {
+        Strafe = 0;
+    }
+    public void LeftButtonDown()
+    {
+        Strafe = Speed;
+        //transform.Translate(Vector3.left * Strafe * Time.deltaTime);
+        Debug.Log("left");
+    }
+
+    public void ButtonUp()
+    {
+        Strafe = 0;
+        Debug.Log("Button Up");
+    }
+
+    public void RightButtonDown()
+    {
+        Strafe = -Speed;
+        //transform.Translate(Vector3.right * Strafe * Time.deltaTime);
+        Debug.Log("right");
+    }
+
+    void Update ()
+    {
+        transform.Translate(Vector3.left * Strafe * Time.deltaTime);
+
+    }
+}
